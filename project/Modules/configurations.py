d5de0fa70a6a626fc1cdd61c498a1b61928de4cd
SurajKande <37841586+SurajKande@users.noreply.github.com>
2020-02-01 14:34:47 +0530

    Add files via upload

---
+++ project/Modules/configurations.py
@@ -0,0 +1,10 @@
+config = {
+       
+  'apiKey': "AIzaSyACAssH2w9e7NJp-vULkwNbIFngGokKNt8",
+  'authDomain': "test-63d5a.firebaseapp.com",
+  'databaseURL': "https://test-63d5a.firebaseio.com/",
+  'projectId': "test-63d5a",
+  'storageBucket': "test-63d5a.appspot.com",
+  'messagingSenderId': "665581966414",
+
+}
---
+++ project/Modules/nav_test.py
@@ -0,0 +1,17 @@
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import numpy as np
+import pickle
+
+df = pd.read_csv('finaldataset.csv')
+xf = df[['keyword', 'grammar', 'qst']]
+
+pickle_in = open('nav_test.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+
+def predict(k, g, q):
+    predicted = clf.predict([[k, g, q]])
+    accuracy = clf.predict_proba([[k, g, q]])
+    print("class[1-9] : " + str(predicted))
+    return predicted
